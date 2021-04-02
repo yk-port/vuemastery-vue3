550575849f894ce780b78bd3517a2c7b1467804f
Tatsuya <yokokura0413@gmail.com>
2021-04-03 08:47:23 +0900

    102-router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{ name: 'event-list' }">Events</router-link> |
+    <router-link :to="{ name: 'about' }">About</router-link>
   </div>
   <router-view />
 </template>
